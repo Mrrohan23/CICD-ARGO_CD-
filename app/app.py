b1d11931f8a3dcb97fc685df84034f224c9a57b5
Mrrohan23 <rohan.p@evds.io>
2026-02-16 23:32:22 +0530

    6rd commit

--- app/app.py
+++ app/app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello DevOps CI/CD update ngroksss!"
+@app.route("/app")
+def hello():
+    return "Sonarscan"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
